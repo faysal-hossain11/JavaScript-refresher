9f667aa8d743ed9098b46211fba42936533db35b
faysal <devfaysalbd@gmail.com>
2024-02-06 21:35:23 +0600

    find duplicate value in a array

--- script.js
+++ script.js
@@ -13,4 +13,12 @@ function isPrime (n) {
     return `${n} is a prime number`
 }
 
-console.log(isPrime(2));
+console.log(isPrime(11));
+
+
+// how to find duplicate value in a array...?
+
+let arr = [1, 3, 4, 6, 4, 6, 7, 9, 11, 3];
+let result = arr.filter((ele, index) => arr.indexOf(ele) !== index);
+
+console.log(result);
