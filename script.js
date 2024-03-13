8e084c66558983ea5db7b7bcf15e96c7ce79a453
faysal <devfaysalbd@gmail.com>
2024-03-13 23:31:44 +0600

    some update

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 // A prime number is a whole number greater than 1. It has excetly two factors, that is, 1 and the  number itself. there is only one even prime number, that is, 2
 
-function isPrime (n) {
+
+
+const isPrime = (n) => {
     if(n < 2) {
-        return `${n} is not a prime number`
+        return false;
     }
 
     for(let i = 2; i < n; i++) {
         if(n % i === 0) {
-            return `${n} is not a pirme number`
+            return false;
         }
     }
-    return `${n} is a prime number`
+    return true;
 }
 
-console.log(isPrime(2));
+for (let i = 1; i <= 100; i++) {
+    if(isPrime(i)) {
+        console.log(`${i} a prime number`);
+    }else{
+        console.log(`${i} is not prime number`);
+    }
+}
+
+// const isPrime = (n) => {
+//     if (n < 2) {
+//       return false;
+//     }
+  
+//     for (let i = 2; i < n; i++) {
+//       if (n % i === 0) {
+//         return false;
+//       }
+//     }
+  
+//     return true;
+//   }
+  
+//   for (let i = 1; i <= 100; i++) {
+//     if (isPrime(i)) {
+//       console.log(`${i} is a prime number`);
+//     } else {
+//       console.log(`${i} is not a prime number`);
+//     }
+//   }
